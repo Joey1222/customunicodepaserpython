c7b1ffbcd02172a6e269e780053ac49096b2d958
jmknu <101217373+Joey1222@users.noreply.github.com>
2023-06-04 16:48:44 -0500

    Changed setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='example',
-    version='0.1.0',
-    packages=find_packages(include=['Paser', 'Paser.*'])
+    name='Custom Unicode Paser',
+    version='1.0.0',
+    description='A Custom unicode paser',
+    author='Joseph Kuntson',
+    author_email='jmknutson2012@gmail.com',
+    url='https://github.com/Joey1222/customunicodepaserpython',
+    packages=find_packages(include=['Paser', 'Paser.*']),
+    install_requires=[
+        ''
+    ],
+    setup_requires=['setuptools'],
+    classifiers=["License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                 "Programming Language :: Python :: 3 :: Only"]
 )
